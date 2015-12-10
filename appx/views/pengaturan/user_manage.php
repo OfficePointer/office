13160637fdd10ab649509298276d0eafa69aa9b5
Arief Setya <email@ariefsetya.com>
2015-12-10 12:04:31 +0700

    ubah letak

--- appx/views/pengaturan/user_manage.php
+++ appx/views/pengaturan/user_manage.php
@@ -29,7 +29,7 @@
             <td><?php echo $this->general->get_sys_div($key['id_division']);?></td>
             <td><?php echo $this->general->get_sys_lev($key['id_level']);?></td>
             <td><?php echo (($key['approved']==1)?"Yes":"No")." (".(($key['mail_type']==0)?"To":"Bcc").")";?></td>
-            <td><a title="Switch Mail Type" href="<?php echo base_url("pengaturan/switch_mail_type/".$key['ID']);?>">ST</a> | <a title="Switch Mail" href="<?php echo base_url("pengaturan/switch_mail/".$key['ID']);?>">SM</a> | <a title="Reset Password" href="<?php echo base_url("pengaturan/reset_password/".$key['ID']);?>">RP</a> | <a title="Edit User" href="<?php echo base_url("pengaturan/user_edit/".$key['ID']);?>">EU</a> <?php if($key['password']=="16d7a4fca7442dda3ad93c9a726597e4"){?>| <a title="SendMail User Account" href="<?php echo base_url("root/sendmail/".$key['ID']);?>">SE</a><?php } ?></td>
+            <td><a title="Switch Mail" href="<?php echo base_url("pengaturan/switch_mail/".$key['ID']);?>">SM</a> | <a title="Switch Mail Type" href="<?php echo base_url("pengaturan/switch_mail_type/".$key['ID']);?>">ST</a> | <a title="Reset Password" href="<?php echo base_url("pengaturan/reset_password/".$key['ID']);?>">RP</a> | <a title="Edit User" href="<?php echo base_url("pengaturan/user_edit/".$key['ID']);?>">EU</a> <?php if($key['password']=="16d7a4fca7442dda3ad93c9a726597e4"){?>| <a title="SendMail User Account" href="<?php echo base_url("root/sendmail/".$key['ID']);?>">SE</a><?php } ?></td>
           </tr>
         <?php
         }
