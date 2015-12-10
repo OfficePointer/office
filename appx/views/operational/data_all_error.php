06a1551feaa1499443eac3d60c5f3c184541adfd
Arief Setya <email@ariefsetya.com>
2015-12-10 13:24:39 +0700

    perbaiki menu error

--- appx/views/operational/data_all_error.php
+++ appx/views/operational/data_all_error.php
@@ -15,13 +15,11 @@
           <tr>
             <th>No.</th>
             <th>Kasus</th>
-            <th>Brand Name</th>
-            <th>Kode Booking</th>
-            <th>Status</th>
-            <th>Error DateTime</th>
-            <th>Solve By</th>
-            <th>Solve Note</th>
-            <th>Solve DateTime</th>
+            <th title="Kode Booking">KB</th>
+            <th title="Error DateTime">Error DT</th>
+            <th title="Solve Note">Solve NT</th>
+            <th title="Solve DateTime">Solve DT</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
@@ -32,13 +30,11 @@
           <tr>
             <td><?php echo $i;?></td>
             <td><?php echo $key['kasus'];?></td>
-            <td><?php echo $this->general->get_member($key['id_mitra']);?></td>
             <td><?php echo $key['kode_booking'];?></td>
-            <td><?php echo $key['status'];?></td>
             <td><?php echo $key['created_at'];?></td>
-            <td><?php echo $this->general->get_user($key['updated_by']);?></td>
             <td><?php echo $key['solve_note'];?></td>
             <td><?php echo $key['updated_at'];?></td>
+            <td><a onclick="openformdetail(<?php echo $key['id_mitra'];?>)" data-toggle="tooltip" data-placement="top" title="Detail Mitra">DTM</a> | <a data-toggle="tooltip" data-placement="left" title="Brand Name : <?php echo $this->general->get_member($key['id_mitra']);?> | Status : <?php echo $key['status'];?> | Error DateTime : <?php echo $key['created_at'];?> | Solve DateTime : <?php echo $key['updated_at'];?> | Solve By : <?php echo $this->general->get_user($key['updated_by']);?>">DTE</a></td>
           </tr>
         <?php
         $i++;
