6a6768290cefdd86cd48e5290b06bd5da9338afd
Nadiannisaqilah <annisaaqilah3@gmail.com>
2016-01-13 15:46:40 +0700

    modul my task (proses)

--- appx/controllers/Servicedesk.php
+++ appx/controllers/Servicedesk.php
@@ -42,7 +42,7 @@ class Servicedesk extends CI_Controller {
 		$this->db->delete('infosys');
 		redirect(base_url('servicedesk/infosys'));
 	}
-	public function infosys_edit($id)
+	public function infosys_edit($id)	
 	{
 		$data = array();
 		$data['info'] = $this->db->get('infosys')->result_array();
@@ -194,5 +194,96 @@ class Servicedesk extends CI_Controller {
 
         <?php
 
+    }
+    public function my_tasks($new = '')
+    {
+    	if($new=="clear"){
+            $this->session->set_userdata('date_tasks','');
+        }
+
+		if($this->input->post('date_tasks')!=""){
+            $daten = explode(" - ",$this->input->post('date_tasks'));
+            $this->session->set_userdata('all_tasks_date_start',$daten[0]);
+            $this->session->set_userdata('all_tasks_date_end',$daten[1]);
+        }else{
+            $this->session->set_userdata('all_tasks_date_start','');
+            $this->session->set_userdata('all_tasks_date_end','');
+        }
+
+		// $data['all_tasks'] = $this->db->query('select 
+		// 		id_ticket, trx_info, vendor.nama,a.`name` as pengirim, 
+		// 		b.`name` as penerima, info, act_budget as jumlah, created_at,`comment`,
+		// 		actionsys.`status` 
+		// 		from actionsys 
+		// 		left join vendor on vendor.id=actionsys.vendor 
+		// 		left join data_user a on a.id=actionsys.id_user 
+		// 		left join data_user b on b.id=actionsys.id_assign
+		// 		')->result_array();
+
+		$this->db->select('id_ticket, trx_info, vendor.nama,a.`name` as pengirim, 
+				b.`name` as penerima, info, act_budget as jumlah, created_at,`comment`,
+				actionsys.`status`');
+		if($this->session->userdata('all_tasks_date_start')!=""){
+            $this->db->where('DATE_FORMAT(actionsys.created_at,"%Y-%m-%d") >=',date_format(date_create($this->session->userdata('all_tasks_date_start')),"Y-m-d"));
+        }
+        if($this->session->userdata('all_tasks_date_end')!=""){
+            $this->db->where('DATE_FORMAT(actionsys.created_at,"%Y-%m-%d") <=',date_format(date_create($this->session->userdata('all_tasks_date_end')),"Y-m-d"));
+        }
+		$this->db->join('vendor','vendor.id=actionsys.vendor','left');
+		$this->db->join('data_user a','a.id=actionsys.id_user','left');
+		$this->db->join('data_user b','b.id=actionsys.id_assign','left');
+		$data = array();
+		$data['my_tasks'] = $this->db->get('actionsys')->result_array();
+		$data['my_tasks'] = $this->db->where('id_user')->get('actionsys')->row_array();
+		//echo "<pre>".print_r($data,1)."<pre>";
+		$this->general->load('servicedesk/my_tasks/data_tasks',$data);
+    }
+    public function expr_my_tasks()
+    {
+    	header('Content-type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment; filename=export_all_error_'.date_format(date_create($this->session->userdata('all_error_date_start')),"Y-m-d").'_'.date_format(date_create($this->session->userdata('all_error_date_end')),"Y-m-d").'_by_'.$this->session->userdata('email').'.xls');
+                    
+		$this->db->select('id_ticket, trx_info, vendor.nama,a.`name` as pengirim, 
+				b.`name` as penerima, info, act_budget as jumlah, created_at,`comment`,
+				actionsys.`status`');
+		if($this->session->userdata('all_tasks_date_start')!=""){
+            $this->db->where('DATE_FORMAT(actionsys.created_at,"%Y-%m-%d") >=',date_format(date_create($this->session->userdata('all_tasks_date_start')),"Y-m-d"));
+        }
+        if($this->session->userdata('all_tasks_date_end')!=""){
+            $this->db->where('DATE_FORMAT(actionsys.created_at,"%Y-%m-%d") <=',date_format(date_create($this->session->userdata('all_tasks_date_end')),"Y-m-d"));
+        }
+		$this->db->join('vendor','vendor.id=actionsys.vendor','left');
+		$this->db->join('data_user a','a.id=actionsys.id_user','left');
+		$this->db->join('data_user b','b.id=actionsys.id_assign','left');
+		$my_tasks = $this->db->get('actionsys',$data)->where('id', $data['id'])->result_array();
+		?>
+      <table>
+        <thead>
+          <tr>
+            <th>IDTicket</th>
+            <th>TrxInfo</th>
+            <th>Info</th>
+            <th>Pengirim</th>
+            <th>Penerima</th>
+            <th>Date Start</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($my_tasks as $key){ ?>
+          <tr>
+            <td><?php echo $key['id_ticket'];?></td>
+            <td><?php echo $key['trx_info'];?></td>
+            <td><?php echo $key['info'];?></td>
+            <td><?php echo $key['pengirim'];?></td>
+            <td><?php echo $key['penerima'];?></td>
+            <td><?php echo $key['created_at'];?></td>
+            <td><?php echo ($key['status']==0)?"Open":(($key['status']==1)?"Hold":(($key['status']==2)?"Done":""));?></td>
+          </tr>
+        <?php } ?>
+        </tbody>
+      </table>
+
+        <?php
     }
 }
---
+++ appx/views/servicedesk/my_tasks/data_tasks.php
@@ -0,0 +1,77 @@
+<!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+      My All Tasks
+      </h1>
+    </section>
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="box box-solid collapsed-box">
+            <div class="box-header" style="cursor: move;">
+              <i class="fa fa-filter"></i>
+
+              <h3 class="box-title">Filter Data</h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-sm" data-widget="collapse"><i class="fa fa-plus"></i>
+                </button>
+              </div>
+            </div>
+            <div class="box-body border-radius-none collapse" style="display: none;">
+
+              <form method="POST" action="<?php echo base_url('servicedesk/my_tasks');?>">
+              <table class="table">
+                <tr>
+                  <td>Date Tasks</td>
+                  <td><input value="<?php echo date_format(date_create($this->session->userdata('all_tasks_date_start')),"m/d/Y");?> - <?php echo date_format(date_create($this->session->userdata('all_tasks_date_end')),"m/d/Y");?>" type="text" name="date_tasks" class="datepicker form-control"></td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td><button class="btn btn-primary">Search</button>
+                  <a class="btn btn-warning" href="<?php echo base_url("servicedesk/my_tasks/clear");?>">Reset</a>
+                  <a class="btn btn-success" href="<?php echo base_url("servicedesk/expr_my_tasks");?>">Export</a></td>
+                </tr>
+              </table>
+              </form>
+
+            </div>
+            <!-- /.box-footer -->
+          </div>
+      <table class="table table-bordered table-striped for_datatables">
+        <thead>
+          <tr>
+            <th>IDTicket</th>
+            <th>TrxInfo</th>
+            <th>Info</th>
+            <th>Pengirim</th>
+            <th>Penerima</th>
+            <th>Date Start</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($my_tasks as $key){ ?>
+          <tr>
+            <td><?php echo $key['id_ticket'];?></td>
+            <td><?php echo $key['trx_info'];?></td>
+            <td><?php echo $key['info'];?></td>
+            <td><?php echo $key['pengirim'];?></td>
+            <td><?php echo $key['penerima'];?></td>
+            <td><?php echo $key['created_at'];?></td>
+            <td><?php echo ($key['status']==0)?"Open":(($key['status']==1)?"Hold":(($key['status']==2)?"Done":""));?></td>
+          </tr>
+        <?php } ?>
+        </tbody>
+      </table>
+      </div>
+      </div>
+      <!-- /.row (main row) -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
