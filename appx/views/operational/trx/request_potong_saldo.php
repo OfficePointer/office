8958223c853c8fd14b0ba1dfa5a946c45bbdb67e
indahnuralifah <indahnuralifah21@@gmail.com>
2016-01-14 11:59:54 +0700

    request potong saldo

---
+++ appx/views/operational/trx/request_potong_saldo.php
@@ -0,0 +1,50 @@
+<!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+      <br><center>Request Potong Saldo</center><br>
+      </h1>
+    </section>
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+
+      <div class="col-md-12">
+      <form method="POST" action="<?php echo base_url("operational/save_request_potong_saldo");?>">
+
+      <table class="table table-bordered table-striped">
+        <thead>
+          <tr>
+            <th>ID Ticket</th>
+            <th>ID Infosys</th>
+            <th>Kode Booking</th>
+            <th>Est Budget</th>
+            <th>Brand Name</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+        <?php
+        foreach ($actionsys as $key) {
+        ?>
+          <tr>
+            <td><?php echo $key['id_ticket'];?></td>
+            <td><?php echo $key['id_infosys'];?></td>
+            <td><?php echo $key['kode_booking'];?></td>
+            <td><?php echo $key['est_budget'];?></td>
+            <td><?php echo $key['brand_name'];?></td>
+            <td><?php echo $key['action'];?></td>
+          </tr>
+        <?php
+        }
+        ?>
+        </tbody>
+      </table>
+        
+      <!-- /.row (main row) -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
