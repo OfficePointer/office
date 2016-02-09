edf9e86d492c41f64fc7abfcfe29a33ee5c4a351
Arief Setya <ariefsetya@live.com>
2016-02-09 17:22:50 +0700

    update klasifikasi no data

--- appx/views/marketing/member_graph.php
+++ appx/views/marketing/member_graph.php
@@ -60,6 +60,7 @@
               <td>
                 <select name="klasifikasi" class="form-control">
                 <option <?php echo ($this->input->post('klasifikasi')=="")?"selected":"";?> value="">-- All --</option>
+                <option <?php echo ($this->input->post('klasifikasi')=="0")?"selected":"";?> value="0">No Data</option>
                 <?php
                 foreach($klasifikasi as $dataklasifikasi){
                 ?>
