9682d79f7f7aa80ab41f09e2cb7b9be80ad3ba31
Arief Setya <ariefsetya@live.com>
2016-09-03 10:34:20 +0700

    fixing email

--- appx/config/email.php
+++ appx/config/email.php
@@ -1,8 +1,8 @@
 <?php
-        $config['smtp_host'] = 'smtp.gmail.com';
+        $config['smtp_host'] = 'ssl://smtp.gmail.com';
         $config['smtp_user'] = 'arief@pointer.co.id';
         $config['smtp_pass'] = '@pointer123';
-        $config['smtp_port'] = '587';
+        $config['smtp_port'] = '465';
         
         $config['protocol'] = 'smtp';
         $config['charset']  = 'iso-8859-1'; //Change this you your preferred charset 
